53648b6598a5f844cb84d4dec889121c7069b9da
gpaul <g.paul@opendo.at>
2014-01-03 11:32:26 +0100

    [TASK] Fixes orientationchange and resize event for rwd slider

--- src/resize-orientationchange.js
+++ src/resize-orientationchange.js
@@ -11,12 +11,9 @@
 			// Remove applied classes and clear object instances first
 			ElementQueriesInstance.revertApplication();
 			ElementQueriesInstance = null;
-
 			RwdObjectsInstance.init(true);
 			RwdObjectsInstance = null;
-
-			//RwdObjectSliderInstance.init(true);
-			//RwdObjectSliderInstance = null;
+			RwdObjectSliderInstance = null;
 
 			// Re-run Element Queries
 			ElementQueriesInstance = new ElementQueries();
@@ -26,9 +23,16 @@
 			RwdObjectsInstance = new RwdObjects();
 			RwdObjectsInstance.init();
 
-			// Re-run Rwd Objects
-			RwdObjectSliderInstance = new RwdObjectSlider();
-			RwdObjectSliderInstance.init();
+			// Re-run Rwd Object Slider
+			var $slider;
+			$('.rwd-object-slider').each(function (i) {
+				$slider = $(this);
+				$slider.find('> .slider-nav .progress').unbind();
+				$slider.find('> .slider-nav .next-button').unbind();
+				$slider.find('> .slider-nav .prev-button').unbind();
+				var RwdObjectSliderInstance = new RwdObjectSlider($slider);
+				RwdObjectSliderInstance.init($slider.find('> .container').find('> .active').index());
+			});
 
 		}, 100);
 	});
--- src/rwd-object-slider.js
+++ src/rwd-object-slider.js
@@ -26,149 +26,130 @@
  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-function RwdObjectSlider() {
+var RwdObjectSliderInstance;
+
+function RwdObjectSlider($rwdObj) {
 	"use strict";
 
 	var self = this;
 
-	this.init = function (revert) {
-		$('.rwd-object-slider').each(function (i) {
-			if (revert) {
+	self.$slider = $rwdObj.find('> .container');
+	self.$sliderItems = self.$slider.find('> .item');
+	self.itemWidth = $rwdObj.width();
+	self.$nextButton = $rwdObj.find('> .slider-nav .next-button');
+	self.$prevButton = $rwdObj.find('> .slider-nav .prev-button');
+	self.$progressBars = $rwdObj.find('> .slider-nav .progress-bar');
+	self.fadeEffect = !!($rwdObj.hasClass('fade'));
 
-			} else {
-				var sliderInstance = new self.Slider($(this));
-				sliderInstance.init(0);
-			}
+	this.init = function (startItemIndex) {
+		self.setStyles(startItemIndex);
+		self.bindEvents();
+	};
 
-			//var sliderInstance = new self.Slider($rwdObj);
-			//if (revert) {
-			//console.log(sliderInstance.$slider.find('> .active').index());
-			//sliderInstance.init(sliderInstance.$slider.find('> .active').index());
-			//sliderInstance.init();
-			//$rwdObj.find('*').unbind();
-			//var activeItemIndex = sliderInstance.$slider.find('> .active').index();
-			//$rwdObj.find('.active').removeClass('active');
-			//console.log(activeItemIndex);
-			//sliderInstance.next(activeItemIndex * sliderInstance.itemWidth * (-1), activeItemIndex);
-			//sliderInstance.$slider.removeClass('is-animated');
-			//} else {
-			//sliderInstance.init(0);
-			//}
+	this.setStyles = function (startItemIndex) {
+		if (!self.fadeEffect) {
+			self.$slider.css({
+				'width': self.$sliderItems.length * self.itemWidth,
+				'left': startItemIndex * self.itemWidth * -1
+			});
+			self.$sliderItems.css('width', self.itemWidth);
+		}
+
+		self.$sliderItems.removeClass('active');
+		self.$slider.find('> .item:nth-child(' + (startItemIndex + 1) + ')').addClass('active');
+		if (startItemIndex === 0) {
+			self.$prevButton.addClass('disabled');
+		} else if (startItemIndex === self.$sliderItems.length) {
+			self.$nextButton.addClass('disabled');
+		}
+		self.$progressBars.each(function () {
+			$(this).find('li:nth-child(' + (startItemIndex + 1) + ')').addClass('active');
+		});
+
+		waitForImagesToLoad(self.$slider, function () {
+			self.$slider.css('height', self.$slider.find('> .item:nth-child(' + (startItemIndex + 1) + ')').outerHeight());
 		});
 	};
 
-	this.Slider = function ($rwdObj) {
-		var self = this;
+	this.next = function (targetXPos, targetItemIndex) {
+		var $activeItem = self.$slider.find('> .active'),
+			$nextItem = self.$slider.find('> .item:nth-child(' + (targetItemIndex + 1) + ')');
 
-		self.$slider = $rwdObj.find('> .container');
-		self.$sliderItems = self.$slider.find('> .item');
-		self.itemWidth = $rwdObj.width();
-		self.$nextButton = $rwdObj.find('> .slider-nav .next-button');
-		self.$prevButton = $rwdObj.find('> .slider-nav .prev-button');
-		self.$progressBars = $rwdObj.find('> .slider-nav .progress-bar');
-		self.fadeEffect = !!($rwdObj.hasClass('fade'));
+		self.$slider.addClass('is-animated');
+		$nextItem.addClass('active').siblings().removeClass('active');
+		self.toggleControlsStateClasses(targetItemIndex);
 
-		this.init = function (startItemIndex) {
-			self.setStyles(startItemIndex);
-			self.bindEvents();
-		};
+		if (self.fadeEffect) {
+			self.$slider.css({ height: $nextItem.outerHeight() });
+		} else {
+			self.$slider.css({ left: targetXPos, height: $nextItem.outerHeight() });
+		}
+	};
 
-		this.setStyles = function (startItemIndex) {
-			if (!self.fadeEffect) {
-				self.$slider.css('width', self.$sliderItems.length * self.itemWidth);
-				self.$sliderItems.css('width', self.itemWidth);
-			}
+	this.isCarouselAnimated = function () {
+		return self.$slider.hasClass('is-animated');
+	};
 
-			self.$sliderItems.removeClass('active');
-			self.$slider.find('> .item:nth-child(' + (startItemIndex + 1) + ')').addClass('active');
+	this.toggleControlsStateClasses = function (targetItemIndex) {
+		if (targetItemIndex === 0) {
 			self.$prevButton.addClass('disabled');
-			self.$progressBars.each(function () {
-				$(this).find('li:nth-child(' + (startItemIndex + 1) + ')').addClass('active');
-			});
+			self.$nextButton.removeClass('disabled');
+		} else if (targetItemIndex === self.$slider.find('> .item:last-child').index()) {
+			self.$nextButton.addClass('disabled');
+			self.$prevButton.removeClass('disabled');
+		} else {
+			self.$nextButton.removeClass('disabled');
+			self.$prevButton.removeClass('disabled');
+		}
+	};
 
-			waitForImagesToLoad(self.$slider, function () {
-				self.$slider.css('height', self.$slider.find('> .item:nth-child(' + (startItemIndex + 1) + ')').outerHeight());
-			});
-		};
+	this.eventNextAndPrevButton = function ($button) {
+		$button.bind('click', function (e) {
+			e.preventDefault();
+			var isNextButton = $(e.target).hasClass('next-button'),
+				targetItemIndex = (isNextButton) ? self.$slider.find('> .active').next().index() : self.$slider.find('> .active').prev().index();
 
-		this.next = function (targetXPos, targetItemIndex) {
-			var $activeItem = self.$slider.find('> .active'),
-				$nextItem = self.$slider.find('> .item:nth-child(' + (targetItemIndex + 1) + ')');
-
-			self.$slider.addClass('is-animated');
-			$nextItem.addClass('active').siblings().removeClass('active');
-			self.toggleControlsStateClasses(targetItemIndex);
-
-			if (self.fadeEffect) {
-				self.$slider.css({ height: $nextItem.outerHeight() });
-			} else {
-				self.$slider.css({ left: targetXPos, height: $nextItem.outerHeight() });
+			if (!self.isCarouselAnimated() && !$(this).hasClass('disabled')) {
+				self.updateProgressBars(targetItemIndex);
+				self.next(targetItemIndex * self.itemWidth * -1, targetItemIndex);
 			}
-		};
+		});
+	};
 
-		this.isCarouselAnimated = function () {
-			return self.$slider.hasClass('is-animated');
-		};
+	this.eventProgressButton = function () {
+		self.$progressBars.find('.progress').bind('click', function (e) {
+			e.preventDefault();
+			var targetItemIndex = $(this).closest('li').index();
 
-		this.toggleControlsStateClasses = function (targetItemIndex) {
-			if (targetItemIndex === 0) {
-				self.$prevButton.addClass('disabled');
-				self.$nextButton.removeClass('disabled');
-			} else if (targetItemIndex === self.$slider.find('> .item:last-child').index()) {
-				self.$nextButton.addClass('disabled');
-				self.$prevButton.removeClass('disabled');
-			} else {
-				self.$nextButton.removeClass('disabled');
-				self.$prevButton.removeClass('disabled');
+			if (!self.isCarouselAnimated() && self.$slider.find('> .active').index() !== targetItemIndex) {
+				self.updateProgressBars(targetItemIndex);
+				self.next(targetItemIndex * self.itemWidth * -1, targetItemIndex);
 			}
-		};
+		});
+	};
 
-		this.eventNextAndPrevButton = function ($button) {
-			$button.bind('click', function (e) {
-				e.preventDefault();
-				var isNextButton = $(e.target).hasClass('next-button'),
-					targetItemIndex = (isNextButton) ? self.$slider.find('> .active').next().index() : self.$slider.find('> .active').prev().index();
+	this.updateProgressBars = function (targetItemIndex) {
+		self.$progressBars.each(function () {
+			$(this).find('li').removeClass('active');
+			$(this).find('li:nth-child(' + (targetItemIndex + 1) + ')').addClass('active');
+		});
+	};
 
-				if (!self.isCarouselAnimated() && !$(this).hasClass('disabled')) {
-					self.updateProgressBars(targetItemIndex);
-					self.next(targetItemIndex * self.itemWidth * -1, targetItemIndex);
-				}
-			});
-		};
+	this.eventTransitionEnd = function () {
+		self.$slider.bind('webkitTransitionEnd transitionend oTransitionEnd', function () {
+			self.$slider.removeClass('is-animated');
+		});
+	};
 
-		this.eventProgressButton = function () {
-			self.$progressBars.find('.progress').bind('click', function (e) {
-				e.preventDefault();
-				var targetItemIndex = $(this).closest('li').index();
-
-				if (!self.isCarouselAnimated() && self.$slider.find('> .active').index() !== targetItemIndex) {
-					self.updateProgressBars(targetItemIndex);
-					self.next(targetItemIndex * self.itemWidth * -1, targetItemIndex);
-				}
-			});
-		};
-
-		this.updateProgressBars = function (targetItemIndex) {
-			self.$progressBars.each(function () {
-				$(this).find('li').removeClass('active');
-				$(this).find('li:nth-child(' + (targetItemIndex + 1) + ')').addClass('active');
-			});
-		};
-
-		this.eventTransitionEnd = function () {
-			self.$slider.bind('webkitTransitionEnd transitionend oTransitionEnd', function () {
-				self.$slider.removeClass('is-animated');
-			});
-		};
-
-		this.bindEvents = function () {
-			self.eventNextAndPrevButton(self.$nextButton);
-			self.eventNextAndPrevButton(self.$prevButton);
-			self.eventProgressButton();
-			self.eventTransitionEnd();
-		};
+	this.bindEvents = function () {
+		self.eventNextAndPrevButton(self.$nextButton);
+		self.eventNextAndPrevButton(self.$prevButton);
+		self.eventProgressButton();
+		self.eventTransitionEnd();
 	};
 }
 
-var RwdObjectSliderInstance = new RwdObjectSlider();
-RwdObjectSliderInstance.init();
+$('.rwd-object-slider').each(function (i) {
+	RwdObjectSliderInstance = new RwdObjectSlider($(this));
+	RwdObjectSliderInstance.init(0);
+});
