3026d7c8d512f34b4cc5efc078696783ea6f0cd7
gpaul <g.paul@opendo.at>
2014-01-18 11:50:36 +0100

    [TASK] Adds grunt setup
    *  Adds concat and minify tasks
    * Adds basic gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function (grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+
+		//Read the package.json (optional)
+		pkg: grunt.file.readJSON('package.json'),
+
+		// Metadata.
+		meta: {
+			srcPath: 'src/development/',
+			deployPath: 'src/'
+		},
+
+		banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+			'* Copyright (c) <%= grunt.template.today("yyyy") %> ',
+
+		// Task configuration.
+		uglify: {
+			files: {
+				src: '<%= meta.deployPath %>rwd-boilerplate.min.js',  // source files mask
+				dest: '<%= meta.deployPath %>',    // destination folder
+				expand: true,    // allow dynamic building
+				flatten: true,   // remove all unnecessary nesting
+				ext: '.min.js'   // replace .js to .min.js
+			}
+		},
+
+		watch: {
+			js:  {
+				files: '<%= meta.srcPath %>*.js',
+				tasks: ['concat', 'uglify']
+			}
+		},
+
+		concat: {
+			options: {
+				stripBanners: true
+			},
+			dist: {
+				src: [
+					'<%= meta.srcPath %>utilities.js',
+					'../element-queries/element-queries.js',
+					'<%= meta.srcPath %>rwd-objects.js',
+					'<%= meta.srcPath %>rwd-object-slider.js',
+					'<%= meta.srcPath %>resize-orientationchange.js',
+					'<%= meta.srcPath %>hide-loading.js'
+				],
+				dest: '<%= meta.deployPath %>rwd-boilerplate.min.js'
+			}
+		}
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	// Default task
+	grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+
+};
