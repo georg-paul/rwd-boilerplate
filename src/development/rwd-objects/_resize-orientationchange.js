19794311b86a13a1294b6442728c84247d428340
gpaul <g.paul@opendo.at>
2014-02-01 09:54:22 +0100

    [TASK] Replaces buggy resize functionality with a hard and bulletproof location reload

--- src/development/rwd-objects/_resize-orientationchange.js
+++ src/development/rwd-objects/_resize-orientationchange.js
@@ -4,47 +4,16 @@
 (function () {
 	'use strict';
 
-	var revertElementQueries = function () {
-		ElementQueriesInstance.revertApplication();
-		ElementQueriesInstance = null;
-	},
-
-		applyElementQueriesAgain = function () {
-			ElementQueriesInstance = new ElementQueries();
-			ElementQueriesInstance.init(true);
-		},
-
-		runRwdObjectsAgain = function () {
-			RwdObjectsInstance = new InitRwdObjects();
-		},
-
-		runRwdSliderAgain = function () {
-			RwdObjectSliderInstance = null; // Clear slider instance first
-
-			var $slider;
-
-			$('.rwd-object-slider').each(function (i) {
-				$slider = $(this);
-				$slider.find('> .slider-nav .progress').unbind();
-				$slider.find('> .slider-nav .next-button').unbind();
-				$slider.find('> .slider-nav .prev-button').unbind();
-				var RwdObjectSliderInstance = new RwdObjectSlider($slider);
-				RwdObjectSliderInstance.init($slider.find('> .container').find('> .active').index(), true);
-				if ($slider.hasClass('autoplay')) {
-					RwdObjectSliderInstance.clearAutoPlayInterval();
-				}
-			});
-		},
-
-		resizeTimeout;
+	var resizeTimeout,
+		wd = window;
 
 	$(window).bind('resize orientationchange', function () {
 		clearTimeout(resizeTimeout);
 		resizeTimeout = setTimeout(function () {
-			revertElementQueries();
-			applyElementQueriesAgain();
-			runRwdObjectsAgain();
-			runRwdSliderAgain();
+			document.querySelector('html').classList.add('rwd-boilerplate-loading'); // show loading animation
+			wd.location.assign(wd.location.href); // go to the URL
+			wd.location.replace(wd.location.href); // go to the URL and replace previous page in history
+			wd.location.reload(false); // reload page from cache
 		}, 100);
 	});
 }());
