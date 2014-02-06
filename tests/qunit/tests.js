f87ec895a341b41ca1c8e303fc9e24adbcd1f5f1
gpaul <g.paul@opendo.at>
2014-02-06 16:48:16 +0100

    [TASK] Adds unit tests

--- tests/qunit/tests.js
+++ tests/qunit/tests.js
@@ -1,14 +1,13 @@
 /*global $ */
 
-// rwd-object-halign
-test("getTotalChildrenWidth returns correct px value", function () {
+test("halign: getTotalChildrenWidth returns correct px value", function () {
 	var $halignTestObject = $('#halign-test-1');
 
 	var HalignTestInstance = new RwdObjectHalign();
 	equal(HalignTestInstance.getTotalChildrenWidth($halignTestObject), 1100);
 });
 
-test("no-side-by-side classes are applied correct", function () {
+test("halign: no-side-by-side classes are applied correct", function () {
 	var $halignTestObject = $('#halign-test-1');
 
 	var HalignTestInstance = new RwdObjectHalign();
@@ -18,6 +17,21 @@ test("no-side-by-side classes are applied correct", function () {
 	ok($('[data-halign-container-id="' + parseInt($halignTestObject.attr('data-halign-id'), 10) + '"]').hasClass('children-no-side-by-side'), true);
 });
 
-test("loading animation has disappeared as expected", function () {
+test("columns: getBreakpoint returns correct value for fixed width columns", function () {
+	var t = new RwdObjectColumns();
+
+	strictEqual(t.getBreakpoint($('<div class="rwd-object-columns-2 fixed-width"></div>'), 999), 0);
+});
+
+test("columns: columns are stacked after reaching a breakpoint", function () {
+	var $columsTestObject = $('#columns-test-1');
+
+	var ColumnsTestInstance = new RwdObjectColumns();
+	ColumnsTestInstance.init();
+
+	ok($columsTestObject.hasClass('stacked-columns'), true);
+});
+
+test("Misc: loading animation has disappeared as expected", function () {
 	equal($('.rwd-boilerplate-loading').length, 0);
 });
