4db82395cf5d01e978ab942fd0b6fdd1834e00e6
gpaul <g.paul@opendo.at>
2014-02-13 15:57:48 +0100

    [FEATURE] Adds resize and orientationchange event handling (slightly different implementation from resize feature in legacy branch)

--- src/development/rwd-objects/_resize.js
+++ src/development/rwd-objects/_resize.js
@@ -5,15 +5,29 @@
 	'use strict';
 
 	var resizeTimeout,
-		wd = window;
+		wd = window,
+		winWidth = window.innerWidth,
+		winHeight = window.innerHeight;
 
-	$(window).bind('resize orientationchange', function () {
-		clearTimeout(resizeTimeout);
-		resizeTimeout = setTimeout(function () {
+	$(wd).bind('resize orientationchange', function () {
+		var refreshPage = function () {
 			$('html').addClass('rwd-boilerplate-loading'); // show loading animation
 			wd.location.assign(wd.location.href); // go to the URL
 			wd.location.replace(wd.location.href); // go to the URL and replace previous page in history
-			wd.location.reload(false); // reload page from cache
-		}, 100);
+			wd.location.reload(); // reload page from cache
+		};
+
+		// New height and width
+		var winNewWidth = window.innerWidth,
+			winNewHeight = window.innerHeight;
+
+		// Compare the new height and width with old one
+		if (winWidth !== winNewWidth || winHeight !== winNewHeight) {
+			window.clearTimeout(resizeTimeout);
+			resizeTimeout = wd.setTimeout(refreshPage, 10);
+		}
+		// Update the width and height
+		winWidth = winNewWidth;
+		winHeight = winNewHeight;
 	});
 }());
